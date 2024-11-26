ab10eb0fea34b1310b87d9c3c8e17270ee6bdf97
ssuarezp <ssuarezp@unal.edu.co>
2024-11-25 20:49:48 -0500

    Commit2

---
+++ src/ejercicio18_actividad2/ejercicio18_actividad2.java
@@ -0,0 +1,17 @@
+//Ejercicio propuesto 18
+package ejercicio18_actividad2;
+
+/**
+ *
+ * @author santi
+ */
+public class ejercicio18_actividad2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
