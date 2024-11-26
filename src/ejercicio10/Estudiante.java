66e4a096891ba3ab7429d2fe4d1706dada75fdd4
ssuarezp <ssuarezp@unal.edu.co>
2024-11-25 23:02:58 -0500

    Commit4

--- src/ejercicio10/Estudiante.java
+++ src/ejercicio10/Estudiante.java
@@ -1,15 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ejercicio13;
-
-/**
- *
- * @author santi
- */
+package ejercicio10;
 public class Estudiante {
-    
     public static double obtener_pagmat(double pat, int est){
     double pagmat=50000;
         if ((pat>2000000)&&(est>3)){
--- src/ejercicio10/ejercicio10_actividad2.java
+++ src/ejercicio10/ejercicio10_actividad2.java
@@ -1,39 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ejercicio13;
+package ejercicio10;
 import java.util.Scanner;
-/**
- *
- * @author santi
- */
-public class ejercicio10_actividad2 {
-    
+public class ejercicio10_actividad2 {  
     public static void main(String[] args) {
         //Ejercicio 10-pág 56
         int ni, est;
         String nombre, apellido;
         double pat, pagmat;
-
+        
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Ingrese el valor del nivel");
+        System.out.println("Ingrese el valor del numero de incripcion");
         ni=entrada.nextInt();
-
+        
         System.out.println("Ingrese nombre");
         nombre=entrada.next();
-
+        
         System.out.println("Ingrese apellido");
         apellido=entrada.next();
-
-        System.out.println("Ingrese el valor del patrimonio");
+        
+        System.out.println("Ingrese patrimonio");
         pat=entrada.nextDouble();
-
+        
         System.out.println("Ingrese el valor del estrato");
         est=entrada.nextInt();
-
+        
         pagmat=Estudiante.obtener_pagmat(pat, est);
-
-        System.out.println("El estudiante con número de inscripción "+ni+" debe pagar "+pagmat);
+        
+        System.out.println("El estudiante "+nombre+" "+apellido+" con numero de inscripcion "+ni+" debe pagar "+pagmat);
     }
 }
